45fcf9dd9a6ad7d3d743a5419ffd40d664543d08
Ryan Sproule <rsproule23@gmail.com>
2026-06-20 12:16:28 -0400

    desc

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,8 +3,7 @@ import { SimpleList, type SimpleListItem } from "@/components/simple-list";
 
 const about: SimpleListItem[] = [
   {
-    title:
-      "Software engineer slash venture investor with deep interest in distributed systems and applied cryptography.",
+    title: "Software engineer, venture investor, startup founder, ex athlete.",
   },
   {
     title: "Github",
